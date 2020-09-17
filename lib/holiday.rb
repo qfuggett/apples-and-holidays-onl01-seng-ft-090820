30b28aef7915068d49bfa8a987ca0b265de78f0d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 17:43:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect do |season, holiday|
-    binding.pry
   end
 
 end
