c6fc0fddbd68a5728bedd94709b2ec97e27d4f2d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 18:18:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,9 @@ def all_holidays_with_bbq(holiday_hash)
     
     holiday.collect do |holiday_name, supplies|
       if supplies.include?("BBQ")
-        holiday_name
+        holiday_name_array = [ ]
+        holiday_name_array << holiday_name
+        holiday_name_array.flatten
       else nil
       end
     end
