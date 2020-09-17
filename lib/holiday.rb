f8f6da74778250c3489aaaf1ed35936e5d4da705
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 18:06:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}"":"
     holiday.each do |holiday_name, supplies|
       puts "  ""#{holiday_name.capitalize}"":"
+      supplies.each do |individual_item|
+        puts " ""#{individual_item}, "
     end
   end
 
