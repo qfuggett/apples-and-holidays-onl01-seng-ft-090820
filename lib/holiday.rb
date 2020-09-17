dda600693ea6954b28d4933e79dfc12752f53931
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 19:38:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
         new_split_names = [ ]
         new_split_names << 
         split_names.capitalize
-        "  #{new_split_names.join(" ")}: #{supplies.join(", ")}"
+        return "  #{new_split_names.join(" ")}: #{supplies.join(", ")}"
       end 
 
     end
